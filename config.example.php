435e59a300ece3b387619031b002854792a04255
contatoprodutivo <111124168+contatoprodutivo@users.noreply.github.com>
2026-07-19 23:33:12 -0300

    Adiciona config.example.php para a aplicação PHP

---
+++ config.example.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+// Copie este arquivo como config.php somente na VPS e preencha os dados reais.
+// Nunca envie config.php ao GitHub.
+$dbHost = 'localhost';
+$dbName = 'NOME_DO_BANCO';
+$dbUser = 'USUARIO_DO_BANCO';
+$dbPass = 'SENHA_DO_BANCO';
